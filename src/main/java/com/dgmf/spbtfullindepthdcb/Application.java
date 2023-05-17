715454d748777e8708e9c456c404274f0776a00c
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-17 17:52:54 +0200

    Renaming of the driver class to "Application". Creation of the HelloController.

--- src/main/java/com/dgmf/spbtfullindepthdcb/Application.java
+++ src/main/java/com/dgmf/spbtfullindepthdcb/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpbtFullInDepthDcbApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpbtFullInDepthDcbApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
