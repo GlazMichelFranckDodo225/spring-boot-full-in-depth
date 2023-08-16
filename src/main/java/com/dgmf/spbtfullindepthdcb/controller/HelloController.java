f905d80883a96cdb4ca67826befcbd265fe1f2eb
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-16 07:39:34 +0200

    Setting up the Endpoint "/" - V2

--- src/main/java/com/dgmf/spbtfullindepthdcb/controller/HelloController.java
+++ src/main/java/com/dgmf/spbtfullindepthdcb/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String helloWorld() {
         return "Welcome to the course !";
     }
