6e6355331990cf210e0a4b36a4789a4cf9435d57
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-16 07:48:20 +0200

    Springboot Devtools

--- src/main/java/com/dgmf/spbtfullindepthdcb/controller/HelloController.java
+++ src/main/java/com/dgmf/spbtfullindepthdcb/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Welcome to the course !";
+        return "Welcome Guys !";
     }
 }
